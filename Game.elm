97a328a0434f5da432844170319f4857d891691f
Théo Zimmermann <theo.zim@free.fr>
2016-04-07 14:57:00 +0000

    Add buttons Restart and Next level but so far they do nothing

---
+++ Game.elm
@@ -0,0 +1,196 @@
+module Game (..) where
+
+
+{-|
+# Game of logic
+
+Copyright Théo Zimmermann 2016. License MPL 2.0
+
+@docs main
+-}
+
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Json.Decode as Json
+import Array exposing (Array)
+import StartApp.Simple as StartApp
+
+
+{- # MAIN -}
+
+
+{-| Run the game. -}
+main : Signal Html
+main =
+  StartApp.start { model = initModel, view = view, update = update }
+
+
+{- # UTILS -}
+
+
+singleton : x -> List x
+singleton x = [x]
+
+
+{- ## Custom events -}
+
+
+messageOn : String -> Signal.Address a -> a -> Attribute
+messageOn name addr msg =
+  on name Json.value (\_ -> Signal.message addr msg)
+
+
+onDragStart = messageOn "dragstart"
+
+
+onDragOver addr msg =
+  onWithOptions
+    "dragover"
+    { preventDefault = True, stopPropagation = False}
+    Json.value
+    (\_ -> Signal.message addr msg)
+
+
+onDrop = messageOn "drop"
+
+
+{- # MODEL -}
+
+
+-- Propositional calculus to start with
+
+
+type Formula
+  = Var String
+  | Impl Formula Formula
+
+
+type alias Model =
+  { context : Array Formula
+  , selected : Maybe Formula
+  , message : String
+  , finished : Bool
+  }
+
+
+initModel : Model
+initModel =
+  { context =
+    [ Impl (Var "A") (Var "B")
+    , Impl (Var "A") (Impl (Var "C") (Var "D"))
+    , Var "A"
+    , Impl (Var "B") (Var "C")
+    ] |> Array.fromList
+  , selected = Nothing
+  , message = "Try moving A on A ⇒ B."
+  , finished = False
+  }
+
+
+{- # VIEW -}
+
+
+addPar : Formula -> String
+addPar f =
+  case f of
+    Var s ->
+      s
+
+    _ ->
+      "(" ++ formulaToString f ++ ")"
+
+formulaToString : Formula -> String
+formulaToString f =
+  case f of
+    Var s ->
+      s
+
+    Impl f1 f2 ->
+       addPar f1 ++ " ⇒ " ++ addPar f2
+
+smallMargin = ("margin" , "4px 0")
+smallPadding = ("padding" , "2px")
+grayBackground = ("background-color" , "gray")
+redBorder = ("border" , "2px solid red")
+fixedHeight = ("height" , "30px")
+italic = ("font-style" , "italic")
+
+
+viewFormula : Signal.Address Action -> Int -> Formula -> Html
+viewFormula address index formula =
+  div
+    [ style
+        [ smallMargin
+        , smallPadding
+        , fixedHeight
+        ]
+    ]
+    [ span
+      [ draggable "true"
+      , onDragStart address <| DragStart (index , formula)
+      , onDragOver address NoOp
+      , onDrop address <| Drop (index , formula)
+      , style
+          [ grayBackground
+          , redBorder
+          , smallPadding
+          ]
+      ]
+      [ text <| formulaToString formula ]
+    ]
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div
+    [ style [("margin" , "10px")]
+    ]
+    [ div [] (Array.toList <| Array.indexedMap (viewFormula address) model.context)
+    , div [ style  [ smallMargin , fixedHeight ] ] [ text "The goal is to get D." ]
+    , div
+        [ style  [ fixedHeight , italic ] ]
+        [ text <| model.message ]
+    ]
+
+
+{- # ACTIONS AND UPDATE -}
+
+
+type Action
+  = NoOp
+  | DragStart (Int, Formula)
+  | Drop (Int, Formula)
+
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    NoOp ->
+      model
+
+    DragStart (index, formula) ->
+      { model | selected = Just formula }
+
+    Drop (index, formula) ->
+      let
+        updatedFormula =
+          case (formula , model.selected) of
+            (Impl f1 f2 , Just f3) ->
+              if f1 == f3 then
+                f2
+              else
+                formula
+
+            _ -> formula
+
+        finished = updatedFormula == Var "D"
+      in
+          { context = Array.set index updatedFormula model.context
+          , selected = Nothing
+          , message =
+              if finished then "You win!" else ""
+          , finished = finished
+          }
+
--- Main.elm
+++ Main.elm
@@ -10,6 +10,7 @@ Copyright Théo Zimmermann 2016. License MPL 2.0
 -}
 
 
+import Game
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
@@ -27,117 +28,23 @@ main =
   StartApp.start { model = initModel, view = view, update = update }
 
 
-{- # UTILS -}
-
-
-singleton : x -> List x
-singleton x = [x]
-
-
-{- ## Custom events -}
-
-
-messageOn : String -> Signal.Address a -> a -> Attribute
-messageOn name addr msg =
-  on name Json.value (\_ -> Signal.message addr msg)
-
-
-onDragStart = messageOn "dragstart"
-
-
-onDragOver addr msg =
-  onWithOptions
-    "dragover"
-    { preventDefault = True, stopPropagation = False}
-    Json.value
-    (\_ -> Signal.message addr msg)
-
-
-onDrop = messageOn "drop"
-
-
 {- # MODEL -}
 
 
--- Propositional calculus to start with
-
-
-type Formula
-  = Var String
-  | Impl Formula Formula
-
-
 type alias Model =
-  { context : Array Formula
-  , selected : Maybe Formula
-  , message : String
+  { game : Game.Model
+  , nextActive : Bool
   }
 
 
 initModel : Model
 initModel =
-  { context =
-    [ Impl (Var "A") (Var "B")
-    , Impl (Var "A") (Impl (Var "C") (Var "D"))
-    , Var "A"
-    , Impl (Var "B") (Var "C")
-    ] |> Array.fromList
-  , selected = Nothing
-  , message = "Try moving A on A ⇒ B."
+  { game = Game.initModel
+  , nextActive = False
   }
 
 
 {- # VIEW -}
-
-
-addPar f =
-  case f of
-    Var s ->
-      s
-
-    _ ->
-      "(" ++ formulaToString f ++ ")"
-
-
-formulaToString f =
-  case f of
-    Var s ->
-      s
-
-    Impl f1 f2 ->
-       addPar f1 ++ " ⇒ " ++ addPar f2
-
-smallMargin = ("margin" , "4px 0")
-bigMargin = ("margin" , "10px 0")
-smallPadding = ("padding" , "2px")
-grayBackground = ("background-color" , "gray")
-redBorder = ("border" , "2px solid red")
-boxHeight = ("height" , "30px")
-italic = ("font-style" , "italic")
-
-
-viewFormula : Signal.Address Action -> Int -> Formula -> Html
-viewFormula address index formula =
-  div
-    [ style
-        [ smallMargin
-        , smallPadding
-        , boxHeight
-        ]
-    ]
-    [ span
-      [ draggable "true"
-      , onDragStart address <| DragStart (index , formula)
-      , onDragOver address NoOp
-      , onDrop address <| Drop (index , formula)
-      , style
-          [ grayBackground
-          , redBorder
-          , smallPadding
-          ]
-      ]
-      [ text <| formulaToString formula ]
-    ]
 
 
 view : Signal.Address Action -> Model -> Html
@@ -145,11 +52,9 @@ view address model =
   div
     [ style [("margin" , "10px")]
     ]
-    [ div [] (Array.toList <| Array.indexedMap (viewFormula address) model.context)
-    , div [ style  [ bigMargin ] ] [ text "The goal is to get D." ]
-    , div
-        [ style  [ bigMargin , italic ] ]
-        [ text <| model.message ]
+    [ Game.view (Signal.forwardTo address GameAction) model.game
+    , button [] [ text "Restart" ]
+    , button [ disabled (not model.nextActive) ] [ text "Next level" ]
     ]
 
 
@@ -158,33 +63,20 @@ view address model =
 
 type Action
   = NoOp
-  | DragStart (Int, Formula)
-  | Drop (Int, Formula)
+  | GameAction Game.Action
 
 
+update : Action -> Model -> Model
 update action model =
   case action of
     NoOp ->
       model
 
-    DragStart (index, formula) ->
-      { model | selected = Just formula }
-
-    Drop (index, formula) ->
+    GameAction action ->
       let
-        updatedFormula =
-          case (formula , model.selected) of
-            (Impl f1 f2 , Just f3) ->
-              if f1 == f3 then
-                f2
-              else
-                formula
-
-            _ -> formula
+        game = Game.update action model.game
       in
-          { context = Array.set index updatedFormula model.context
-          , selected = Nothing
-          , message =
-              if updatedFormula == Var "D" then "You win!" else ""
-          }
+        { game = game
+        , nextActive = game.finished
+        }
 
