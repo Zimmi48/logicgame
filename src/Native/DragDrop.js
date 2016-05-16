78283c1f4ef6cf56eaeaeaf28b57d691303e0a36
Théo Zimmermann <theo.zimmermann@ens.fr>
2016-05-16 16:47:34 +0200

    Re-indent and start porting Native module to 0.17

--- src/Native/DragDrop.js
+++ src/Native/DragDrop.js
@@ -1,81 +1,68 @@
 /* Code derived from  https://github.com/kurtharriger/elm-battleship */
 
-Elm.Native.DragDrop = {};
+var _Zimmi48$logicgame$Native_DragDrop = function() {
+    localRuntime.Native = localRuntime.Native || {};
+    localRuntime.Native.DragDrop = localRuntime.Native.DragDrop || {};
+    if (localRuntime.Native.DragDrop.values) {
+	return localRuntime.Native.DragDrop.values;
+    }
+    
+    var Json = Elm.Native.Json.make(localRuntime);
+    var Signal = Elm.Native.Signal.make(localRuntime);
 
-Elm.Native.DragDrop.make = function(localRuntime) {
-	localRuntime.Native = localRuntime.Native || {};
-	localRuntime.Native.DragDrop = localRuntime.Native.DragDrop || {};
-	if (localRuntime.Native.DragDrop.values) {
-		return localRuntime.Native.DragDrop.values;
-	}
+    function property(key, value) {
+	return {
+	    key: key,
+	    value: value
+	};
+    }
+    
+    function onDragStart(options, decoder, createMessage) {
+	var name = 'dragstart';
+	function eventHandler(event) {
+	    event.dataTransfer.setDragImage(event.target, 0, 0);
+	    // This stupid setData call is necessary to make drag and drop work on FF
+	    event.dataTransfer.setData("text","");
 
-  var Json = Elm.Native.Json.make(localRuntime);
-  var Signal = Elm.Native.Signal.make(localRuntime);
-  var VirtualDom = Elm.Native.VirtualDom.make(localRuntime);
-
-  function property(key, value)
-	{
-		return {
-			key: key,
-			value: value
-		};
-	}
-
-  function onDragStart(options, decoder, createMessage)
-	{
-    var name = 'dragstart';
-		function eventHandler(event)
-		{
-      event.dataTransfer.setDragImage(event.target, 0, 0);
-      // This stupid setData call is necessary to make drag and drop work on FF
-      event.dataTransfer.setData("text","");
-
-			var value = A2(Json.runDecoderValue, decoder, event);
-			if (value.ctor === 'Ok')
-			{
-				if (options.stopPropagation)
-				{
-					event.stopPropagation();
-				}
-				if (options.preventDefault)
-				{
-					event.preventDefault();
-				}
-				Signal.sendMessage(createMessage(value._0));
-			}
+	    var value = A2(Json.runDecoderValue, decoder, event);
+	    if (value.ctor === 'Ok') {
+		if (options.stopPropagation) {
+		    event.stopPropagation();
 		}
-		return property('on' + name, eventHandler);
+		if (options.preventDefault) {
+		    event.preventDefault();
+		}
+		Signal.sendMessage(createMessage(value._0));
+	    }
 	}
+	return property('on' + name, eventHandler);
+    }
 
-  function onDragOver(options, decoder, createMessage)
+    function onDragOver(options, decoder, createMessage) {
+	var name = 'dragover';
+	function eventHandler(event)
 	{
-    var name = 'dragover';
-		function eventHandler(event)
-		{
-
-			var value = A2(Json.runDecoderValue, decoder, event);
-			if (value.ctor === 'Ok')
-			{
-				if (options.stopPropagation)
-				{
-					event.stopPropagation();
-				}
-				if (options.preventDefault)
-				{
-					event.preventDefault();
-				}
-                if (typeof(options.dropEffect) == "string")
-                {
+	    
+	    var value = A2(Json.runDecoderValue, decoder, event);
+	    if (value.ctor === 'Ok') {
+		if (options.stopPropagation) {
+		    event.stopPropagation();
+		}
+		if (options.preventDefault) {
+		    event.preventDefault();
+		}
+                if (typeof(options.dropEffect) == "string") {
                     event.dataTransfer.dropEffect = options.dropEffect;
                 }
-				Signal.sendMessage(createMessage(value._0));
-			}
-		}
-		return property('on' + name, eventHandler);
+		Signal.sendMessage(createMessage(value._0));
+	    }
 	}
+	return property('on' + name, eventHandler);
+    }
 
-  return localRuntime.Native.Json.values = {
-    onDragStart: F3(onDragStart),
-    onDragOver: F3(onDragOver)
-  };
-};
+    return {
+	onDragStart: F3(onDragStart),
+	onDragOver: F3(onDragOver)
+    };
+
+}();
